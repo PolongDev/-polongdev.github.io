58271b77c9126fa1b0d7ca30484e967a88743105
Kenneth L. Perez <148478983+PolongDev@users.noreply.github.com>
2025-01-23 09:27:51 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import About from "./pages/about";
+import Contact from "./pages/contacts";
+import Hero from "./pages/hero";
+import Header from "./parts/header";
+
+function App() {
+  return (
+    <div className="flex flex-col dark:bg-[#212129] dark:text-white w-dvw h-dvh">
+      <Header className={``} />
+      <div id="base" className="w-full h-full overflow-scroll">
+        <Hero
+          id="main"
+          className="dark:bg-[#212129] dark:text-white flex flex-col box-border items-center justify-center h-full w-full"
+        />
+        <About
+          id="about"
+          className="dark:bg-[#212129] dark:text-white flex flex-col box-border items-center justify-center h-full w-full"
+        />
+        <Contact
+          id="contact"
+          className="dark:bg-[#212129] dark:text-white flex flex-col box-border items-center justify-center h-full w-full"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/honeycomb.tsx
@@ -0,0 +1,17 @@
+import Hexagon from "./hexagon";
+
+interface _props {
+  className: string;
+}
+
+function HoneyComb(props: _props) {
+  return (
+    <div className={`flex ${props.className}`}>
+      >
+        Web Sample
+      </Hexagon>
+    </div>
+  );
+}
+
+export default HoneyComb;
